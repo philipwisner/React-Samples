fbfacc34974bfccf2caf7607d3e950d218d90129
philipwisner <philipwisner@gmail.com>
2018-04-15 22:48:55 -0400

    more routes

--- Learning Sample/routing-sample/src/App.js
+++ Learning Sample/routing-sample/src/App.js
@@ -11,12 +11,22 @@ const Home = () => (
   <h2>Home</h2>
 )
 
+const About = () => (
+  <h2>About</h2>
+)
+
+const Topics = () => (
+  <h2>Topics</h2>
+)
+
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="App">
            <Route path ='/' component={Home} />
+           <Route path ='/about' component={About}/>
+           <Route path ='/topics' component={Topics}/>
         </div>
       </Router>
     );
