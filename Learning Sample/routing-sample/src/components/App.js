c60dbe268cd7d04cdaa472570bbebc7aa048e020
philipwisner <philipwisner@gmail.com>
2018-04-17 00:07:07 -0400

    restructure

--- Learning Sample/routing-sample/src/components/App.js
+++ Learning Sample/routing-sample/src/components/App.js
@@ -1,60 +1,14 @@
 import React, { Component } from 'react';
 import Home from './Home';
-import '/../App.css';
+import About from './About';
+import Topics from './Topics';
+import '../css/App.css';
 import {
     BrowserRouter as Router,
     Route,
     Link,
   } from 'react-router-dom';
 
-
-const Home = () => (
-  <div>
-    <h2>Home</h2>
-  </div>
-)
-
-const About = () => (
-  <div>
-    <h2>About</h2>
-  </div>
-)
-
-const Topic = ({ match }) => (
-  <div>
-    <h3>{match.params.topicId}</h3>
-  </div>
-)
-
-const Topics = ({ match }) => (
-  <div>
-    <h2>Topics</h2>
-    <ul>
-      <li>
-        <Link to={`${match.url}/rendering`}>
-          Rendering with React
-        </Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/components`}>
-          Components
-        </Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/props-v-state`}>
-          Props V. State
-        </Link>
-      </li>
-    </ul>
-    
-    <Route path={`${match.url}/:topicId`} component={Topic} />
-    <Route exact path={match.url} render={() => (
-        <h3>Please Select a Topic</h3>
-      )}/>
-  </div>
-)
-
-//Using TopicID because we are rendering the same component for each route
 
 class App extends Component {
   render() {
@@ -69,8 +23,8 @@ class App extends Component {
           
           <hr/>
            <Route exact path ='/' component={Home} />
-           <Route path ='/about' component={About}/>
-           <Route path ='/topics' component={Topics}/>
+           <Route path ='/about' component={About} />
+           <Route path ='/topics' component={Topics} />
         </div>
       </Router>
     );
--- Learning Sample/routing-sample/src/components/Topics.js
+++ Learning Sample/routing-sample/src/components/Topics.js
@@ -1,5 +1,42 @@
-import React from 'react';
+import React from 'react'
+import {
+  Link,
+  Route
+} from 'react-router-dom'
 
-export default function Home () {
-  return <div>Topics</div>
+function Topic ({match}) {
+  return <h3>{match.params.topicId}</h3>
 }
+
+export default function Topics ({match}) {
+  return (
+    <div>
+      <h2>Topics</h2>
+      <ul>
+        <li>
+          <Link to={`${match.url}/rendering`}>
+            Rendering with React
+          </Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/components`}>
+            Components
+          </Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/props-v-state`}>
+            Props V. State
+          </Link>
+        </li>
+      </ul>
+
+      <Route path={`${match.url}/:topicId`} component={Topic} />
+      <Route exact path={match.url} render={() => (
+          <h3>Please Select a Topic</h3>
+        )}/>
+    </div>
+  )
+}
+
+
+//Using TopicID because we are rendering the same component for each route
